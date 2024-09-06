0bb2cc8b9541b9efb51127d60e031b951cff2f2f
Alberte <albo0002@stud.kea.dk>
2024-09-06 08:59:19 +0200

    css finpudset + rabat og udsolgt tilføjet til enkelt produkt

--- produkt.js
+++ produkt.js
@@ -15,21 +15,39 @@ function visProduct(product){
     document.querySelector("img").src= `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
     document.querySelector("img").alt= product.productdisplayname;
 
-    if (product.soldout) {
-        clone.querySelector("article").classList.add("soldOut"); 
-        clone.querySelector(".soldOut2").style.display = "block"; 
+
+        // Håndterer "Udsolgt"-status
+        if (product.soldout) {
+          document.querySelector(".soldOut2").style.display = "block"; // Viser "Udsolgt"-mærke
       } else {
-        clone.querySelector(".soldOut2").style.display = "none"; 
+          document.querySelector(".soldOut2").style.display = "none"; // Skjuler "Udsolgt"-mærke
       }
+  
+      // Håndterer rabat
+      if (product.discount > 0) {
+          document.querySelector(".discounted").style.display = "block"; // Viser rabat-sektionen
+          const discountedPrice = Math.round(product.price - (product.price * product.discount) / 100);
+          document.querySelector(".discounted p span").textContent = discountedPrice;
+          document.querySelector(".discounted p+p span").textContent = product.discount;
+      } else {
+          document.querySelector(".discounted").style.display = "none"; // Skjuler rabat-sektionen, hvis ingen rabat
+      }
+  
+    // if (product.soldout) {
+    //     clone.querySelector("article").classList.add("soldOut"); 
+    //     clone.querySelector(".soldOut2").style.display = "block"; 
+    //   } else {
+    //     clone.querySelector(".soldOut2").style.display = "none"; 
+    //   }
    
       
-      if (product.discount) {
-        clone.querySelector("article").classList.add("discount");
-        clone.querySelector(".discounted p span").textContent = Math.round(product.price - (product.price * product.discount) / 100);
-        clone.querySelector(".discounted p+p span").textContent = product.discount;
-      } else {
-        clone.querySelector(".discounted").style.display = "none";  
-      }
+    //   if (product.discount) {
+    //     clone.querySelector("article").classList.add("discount");
+    //     clone.querySelector(".discounted p span").textContent = Math.round(product.price - (product.price * product.discount) / 100);
+    //     clone.querySelector(".discounted p+p span").textContent = product.discount;
+    //   } else {
+    //     clone.querySelector(".discounted").style.display = "none";  
+    //   }
      
 }
 
